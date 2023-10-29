cbe55c590b88e5acf3e78ab0ddc1cf805013dfaf
AhmedA005 <117976612+AhmedA005@users.noreply.github.com>
2023-10-29 09:39:46 +0200

    Add files via upload

---
+++ A2_S2_20220443_6a.cpp
@@ -0,0 +1,26 @@
+// File: A2_S2_20220443_6a.cpp
+// Purpose: Convert decimal numbers to binary using recursion
+// Author: Ahmed Ahmed Mostafa
+// Section: S1,2
+// ID: 20220443
+// TA: Maya Ahmed Kamal
+// Date: 27 Oct 2023
+#include <bits/stdc++.h>
+using namespace std;
+string s="";
+void binary(int n){
+    if (n==0)
+        return;
+    if (n%2) s+='1';
+    else s+='0';
+    binary(n/2);
+}
+int main(){
+    int n;
+    cin>>n;
+    binary(n);
+    reverse(s.begin(),s.end());
+    cout<<s;
+}
+
+
