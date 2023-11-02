698bc7b1c642ca8d0ea0f0dbdb532ba513699e0c
AhmedA005 <117976612+AhmedA005@users.noreply.github.com>
2023-11-02 16:03:53 +0200

    Update BigReal.cpp

--- A2_Task2_S2_20220443_20220183_20221130/BigReal.cpp
+++ A2_Task2_S2_20220443_20220183_20221130/BigReal.cpp
@@ -7,7 +7,6 @@ bool BigReal::isValid(string realNumber) {
         return true;
     else return false;
 }
-
 BigReal::BigReal(string realNumber) {
     if (isValid(realNumber)){
         if (realNumber[0]=='-' || realNumber[0]=='+'){
@@ -33,7 +32,7 @@ ostream &operator<<(ostream &out, BigReal num) {
 }
 
 bool BigReal::operator==(BigReal &other) {
-    if(sign==other.sign) {
+    if (sign == other.sign) {
         while (integer.size() > other.integer.size()) {
             other.integer = '0' + other.integer;
         }
@@ -50,81 +49,117 @@ bool BigReal::operator==(BigReal &other) {
             return false;
         } else
             return true;
-    }
-    else
+    } else
         return false;
 }
 
 bool BigReal::operator<(BigReal &other) {
-    if(sign == '-' and other.sign == '+')
+    if (sign == '-' and other.sign == '+')
         return true;
-    else if(sign == '+' and other.sign == '-')
+    else if (sign == '+' and other.sign == '-')
         return false;
 
-    while (integer.size() > other.integer.size()){
-        other.integer = '0'+other.integer;
+    while (integer.size() > other.integer.size()) {
+        other.integer = '0' + other.integer;
     }
-    while (integer.size() < other.integer.size()){
-        integer = '0'+integer;
+    while (integer.size() < other.integer.size()) {
+        integer = '0' + integer;
     }
-    while (fraction.size() > other.fraction.size()){
+    while (fraction.size() > other.fraction.size()) {
         other.fraction += '0';
     }
-    while (fraction.size() < other.fraction.size()){
+    while (fraction.size() < other.fraction.size()) {
         fraction += '0';
     }
-    if(integer == other.integer and fraction == other.fraction)
+    if (integer == other.integer and fraction == other.fraction)
         return false;
 
-    if(sign == '+' and other.sign == '+'){
+    if (sign == '+' and other.sign == '+') {
 
         for (int i = 0; i < integer.size(); ++i) {
-            if(integer[i] > other.integer[i])
+            if (integer[i] > other.integer[i])
                 return false;
         }
 
         for (int i = 0; i < fraction.size(); ++i) {
-            if(fraction[i] > other.fraction[i])
+            if (fraction[i] > other.fraction[i])
                 return false;
         }
-
     }
 
-    if(sign == '-' and other.sign == '-'){
+    if (sign == '-' and other.sign == '-') {
 
         for (int i = 0; i < integer.size(); ++i) {
-            if(integer[i] < other.integer[i])
+            if (integer[i] < other.integer[i])
                 return false;
         }
 
         for (int i = 0; i < fraction.size(); ++i) {
-            if(fraction[i] < other.fraction[i])
+            if (fraction[i] < other.fraction[i])
                 return false;
         }
-
     }
     return true;
 }
 
 bool BigReal::operator>(BigReal &other) {
-    BigReal one(integer+'.'+fraction);
-    if(one < other or one == other)
+    BigReal one(integer + '.' + fraction);
+    if (one < other or one == other)
         return false;
     else
         return true;
 }
-bool BigReal::operator>=(BigReal &other) {
-    BigReal one(integer+'.'+fraction);
-    if (one < other)
-        return false;
-    else
-    return true;
-}
 
-bool BigReal::operator<=(BigReal &other) {
-    BigReal one(integer+'.'+fraction);
-    if (one > other)
-        return false;
-    else
-    return true;
+BigReal BigReal::operator+(BigReal &other) {
+    BigReal sum("0.0");
+    if (sign == other.sign) {
+        while (integer.size() > other.integer.size()) {
+            other.integer = '0' + other.integer;
+        }
+        while (integer.size() < other.integer.size()) {
+            integer = '0' + integer;
+        }
+        while (fraction.size() > other.fraction.size()) {
+            other.fraction += '0';
+        }
+        while (fraction.size() < other.fraction.size()) {
+            fraction += '0';
+        }
+        sum = other;
+        int carry = 0;
+        for (int i = fraction.size() - 1; i >= 0; --i) {
+            int x = fraction[i] - '0';
+            int y = other.fraction[i] - '0';
+            int z = x + y + carry;
+            if (z >= 10) {
+                carry = 1;
+                z -= 10;
+            } else
+                carry = 0;
+            z += '0';
+            sum.fraction[i] = z;
+            if (i > 0) sum.fraction[i - 1] += (carry + '0');
+        }
+
+        for (int i = integer.size() - 1; i >= 0; --i) {
+            int x = integer[i] - '0';
+            int y = other.integer[i] - '0';
+            int z = x + y + carry;
+            if (z >= 10) {
+                carry = 1;
+                z -= 10;
+            } else
+                carry = 0;
+            z += '0';
+            sum.integer[i] = z;
+            if (i > 0) sum.integer[i - 1] += (carry + '0');
+        }
+        if (carry == 1) {
+            sum.integer = '1' + sum.integer;
+            carry = 0;
+        }
+    } else {
+        //        minus here
+    }
+    return sum;
 }
