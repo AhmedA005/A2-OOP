e1f30fea9cb2aae715113b006c8ec9e10c826742
AhmedA005 <117976612+AhmedA005@users.noreply.github.com>
2023-11-04 18:26:02 +0200

    Update InclusiveSpeech.cpp

--- A2_S2_20220443_2/InclusiveSpeech.cpp
+++ A2_S2_20220443_2/InclusiveSpeech.cpp
@@ -6,8 +6,8 @@ void InclusiveSpeech::setSpeech(string speech) {
     Speech=speech;
 }
 
-void InclusiveSpeech::modifySpeech(string& Speech) {
-    map<string, string> replacements = {
+void InclusiveSpeech::modifySpeech(string& Speech){
+        map<string, string> replacements = {
             {"he", "he or she"},
             {"his", "his or hers"},
             {"him", "him or her"},
@@ -15,7 +15,15 @@ void InclusiveSpeech::modifySpeech(string& Speech) {
             {"He", "He or she"},
             {"His", "His or hers"},
             {"Him", "Him or her"},
-            {"Himself", "Himself or herself"}
+            {"Himself", "Himself or herself"} ,
+            {"he?", "he or she?"},
+            {"his?", "his or hers?"},
+            {"him?", "him or her?"},
+            {"himself?", "himself or herself?"},
+            {"He?", "He or she?"},
+            {"His?", "His or hers?"},
+            {"Him?", "Him or her?"},
+            {"Himself?", "Himself or herself?"}
     };
 
     stringstream ss(Speech);
