4e6fe525cf5e865957c12221f1fd6f239227b8ea
GodZilla57 <148370503+GodZilla57@users.noreply.github.com>
2023-11-05 13:17:38 +0200

    Update A2_S2_20220183_10.cpp

--- A2_S2_20220183_10/A2_S2_20220183_10.cpp
+++ A2_S2_20220183_10/A2_S2_20220183_10.cpp
@@ -1,5 +1,5 @@
 // File: A2_S2_20220183_10.cpp
-// Purpose: find all the prime numbers up to some upper limit N.
+// Purpose: Message Altering to Avoid Censorship.
 // Author: Abdelrahman Wael
 // Section: S1,2
 // ID: 20220183
@@ -19,13 +19,16 @@ int main() {
     outfile.open("out.txt",ios::out | ios::trunc);
     string line;
     string word;
-    //int space = 0;
+    //Repeat the same algorithm for every line
     while(getline(infile,line)){
         for (int i = 0; i < line.size(); ++i) {
+            //See the first space index. all letters before it is considered one word
             if(line[i] == ' '){
                 word = line.substr(0,i);
+                //erase the first word and start the loop from i = 0 again
                 line.erase(0,i+1);
                 i=0;
+                //Alterning the word if it is censored and store it in the output file
                 outfile << filter(word)<<' ';
             }
         }
@@ -48,6 +51,8 @@ string filter(string word){
             if(line[i] == ' '){
                 spacepos=i;
             }
+            //compare the word with every censored word in the table  file
+            //if the word is equal to any censored word replace it with an alternative
             string band_word = line.substr(0,spacepos);
             if(word == band_word){
                 alternative = line.substr(spacepos+1,line.size()-1);
