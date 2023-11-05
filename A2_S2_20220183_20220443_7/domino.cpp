ca3bbdf48f775d25da1a5e5148b85c453695a8ad
GodZilla57 <148370503+GodZilla57@users.noreply.github.com>
2023-11-05 15:30:51 +0200

    Update domino.cpp

--- A2_S2_20220183_20220443_7/domino.cpp
+++ A2_S2_20220183_20220443_7/domino.cpp
@@ -3,7 +3,7 @@
 #include <deque>
 #include "domino.h"
 using namespace std;
-vector<domino>ans;
+deque<domino>ans;
 domino ::domino()
 {
     leftdots = 0;
@@ -30,29 +30,38 @@ ostream & operator<<(ostream & out,domino & d)
 //Overloading the operator == To take two dominoes and return them can either form a chain or not
 bool  operator==(domino d, domino d1)
 {
-    return (d.rightdots == d1.leftdots or d.leftdots == d1.rightdots);
+    return (d.rightdots == d1.leftdots);
 }
 
-bool FormsDominoChain(vector <domino> v,vector <domino>& ans )
+bool FormsDominoChain(vector <domino> v,deque <domino>& ans )
 {
     //check every time if the vector is empty
     if (v.empty()) return true;
     //loop on the vector and see if each domino has a match
     //and store them in vector ans
     for (int i = 0; i < v.size(); ++i) {
-        if (ans.empty()||ans.back()==v[i]){
-            ans.push_back(v[i]);
-            domino tmp=v[i];
-            v.erase(v.begin()+i);
-            if (FormsDominoChain(v,ans)) return true;
+        if (ans.empty() || ans.back()==v[i]){
+                ans.push_back(v[i]);
+                domino tmp = v[i];
+                v.erase(v.begin() + i);
+                if (FormsDominoChain(v, ans)) return true;
 
-            v.insert(v.begin()+i,tmp);
-            ans.pop_back();
+                v.insert(v.begin() + i, tmp);
+                ans.pop_back();
+            }
+            else if(v[i] == ans.back()) {
+                ans.push_front(v[i]);
+                domino tmp = v[i];
+                v.erase(v.begin() + i);
+                if (FormsDominoChain(v, ans)) return true;
+
+                v.insert(v.begin() + i, tmp);
+                ans.pop_front();
+            }
         }
-    }
     return false;
 }
-void print_domino(vector<domino> ans) {
+void print_domino(deque <domino> ans) {
     for (int i = 0; i < ans.size(); ++i) {
         cout<<ans[i].leftdots<<'|'<<ans[i].rightdots;
         if (i!=ans.size()-1) cout<<" - ";
