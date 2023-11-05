6a4137cfb43642c97d51aa862c863aca752a2081
GodZilla57 <148370503+GodZilla57@users.noreply.github.com>
2023-11-05 15:30:36 +0200

    Update domino.h

--- A2_S2_20220183_20220443_7/domino.h
+++ A2_S2_20220183_20220443_7/domino.h
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <deque>
 using namespace std;
 
 #ifndef A2_S2_20220183_7_DOMINO_H
@@ -14,8 +16,8 @@ public:
     friend istream & operator>>(istream & input,domino & d);
     friend ostream & operator<<(ostream & out,domino & d);
     friend bool  operator==(domino d , domino d1);
-    friend bool FormsDominoChain(vector <domino> v,vector<domino>& ans );
-    friend void print_domino( vector <domino> v);
+    friend bool FormsDominoChain(vector <domino> v,deque <domino>& ans );
+    friend void print_domino( deque <domino> v);
 };
 
 
