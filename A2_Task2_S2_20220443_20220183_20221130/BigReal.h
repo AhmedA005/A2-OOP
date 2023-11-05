d2f5c49a6521eb10ec57805fe9663937e87f55c9
AhmedA005 <117976612+AhmedA005@users.noreply.github.com>
2023-11-05 19:09:17 +0200

    Update BigReal.h

--- A2_Task2_S2_20220443_20220183_20221130/BigReal.h
+++ A2_Task2_S2_20220443_20220183_20221130/BigReal.h
@@ -12,20 +12,15 @@ class BigReal {
 public:
     BigReal(double number = 0.0);
     BigReal(string realNumber);
-    BigReal(const BigReal &other);
-    BigReal &operator=(BigReal &other);
-    void setNum(string number);
     BigReal operator+(BigReal &other);
     BigReal operator-(BigReal &other);
-    void print(BigReal&);
 
     bool operator<(BigReal &other);
     bool operator>(BigReal &other);
     bool operator==(BigReal &other);
-    bool operator>=(BigReal &other);
-    bool operator<=(BigReal &other);
+    BigReal Modify(BigReal &other);
 
-    friend ostream &operator<<(ostream &out, BigReal num);
-    friend istream &operator>>(istream &in, BigReal num);
+    friend ostream &
+    operator<<(ostream &out, BigReal num);
 };
 #endif//BIGREAL_BIGREAL_H
