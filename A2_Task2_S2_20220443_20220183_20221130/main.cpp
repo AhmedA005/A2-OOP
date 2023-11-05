8731aec59551384ed0bf4f05731354744cf86655
AhmedA005 <117976612+AhmedA005@users.noreply.github.com>
2023-11-05 19:24:11 +0200

    Update main.cpp

--- A2_Task2_S2_20220443_20220183_20221130/main.cpp
+++ A2_Task2_S2_20220443_20220183_20221130/main.cpp
@@ -1,3 +1,10 @@
+// File: main.cpp
+// Purpose: Store big numbers that are not supported in c++, and perform some operations on it.
+// Author: Abdelrahman Wael & Mohamed Mahmoud & Ahmed Ahmed
+// Section: S1,2
+// ID: 20220183 , 20221130 , 20220443
+// TA: Maya Ahmed Kamal
+// Date: 01 Nov 2023
 #include <iostream>
 using namespace std;
 #include "BigReal.cpp"
