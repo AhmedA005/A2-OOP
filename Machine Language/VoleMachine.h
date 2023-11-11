98f47ea335174d45e2bc13c7bcac9b632dd6ed50
AhmedA005 <117976612+AhmedA005@users.noreply.github.com>
2023-11-11 12:00:21 +0200

    Update VoleMachine.h

--- Machine Language/VoleMachine.h
+++ Machine Language/VoleMachine.h
@@ -3,14 +3,6 @@ using namespace std;
 #ifndef MACHINE_LANGUAGE_VOLEMACHINE_H
 #define MACHINE_LANGUAGE_VOLEMACHINE_H
 
-class Memory {
-    string s;
-};
-
-class Register {
-    string s;
-};
-
 class Machine {
     vector<string> reg{16};
     map<string, string> mem;
@@ -32,18 +24,23 @@ public:
         ifstream file(filename);
         if (file.is_open()) {
             string line;
-            int i = 0;
             string PC = "00";
             while (getline(file, line)) {
+
                 mem[PC] = line;
                 int pcValue = stoi(PC, nullptr, 16);// Convert the hex string to an integer
                 pcValue += 2;                       // Increment the value
                 stringstream ss;
                 ss << std::hex << pcValue;// Convert the integer back to hex
                 PC = ss.str();
+
                 while (PC.size() < 2) {
                     PC = "0" + PC;// Ensure two characters in PC
                 }
+                if(PC[1]>=97 and PC[1]<=122){
+                    PC[1]-=32;
+                }
+            cout<<PC<<endl;
             }
             file.close();
         }
@@ -56,14 +53,13 @@ public:
             int location;
             stringstream ss(instruction.substr(2, 2));
 
-            ss >> std::hex >> location;
             if (ss.str().size() == 1) {
-                // Prepend '0' for single-digit hexadecimal values
                 ss.str("0" + ss.str());
             }
             if (mem[ss.str()].size() > 2)
                 mem[ss.str()].erase(0, 2);
             reg[instruction[1] - '0'] = mem[ss.str()];
+
         } else if (instruction[0] == '2') {
             reg[instruction[1] - '0'] = instruction.substr(2, 2);
         } else if (instruction[0] == '3') {
