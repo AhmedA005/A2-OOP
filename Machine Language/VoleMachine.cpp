62ce3a6e477c8dbeeba41038847a9a000e00b350
GodZilla57 <148370503+GodZilla57@users.noreply.github.com>
2023-11-12 22:02:32 +0200

    Update VoleMachine.cpp

--- Machine Language/VoleMachine.cpp
+++ Machine Language/VoleMachine.cpp
@@ -47,14 +47,27 @@ void Machine::processInstruction(string &instruction) {
         if (ss.str().size() == 1) {
             ss.str("0" + ss.str());
         }
-        if (mem[ss.str()].size() > 2)
+       if (mem[ss.str()].size() > 2)
             mem[ss.str()].erase(0, 2);
+       if(mem[ss.str()][0] == ' ')
+           mem[ss.str()].erase(0,1);
         reg[instruction[1] - '0'] = mem[ss.str()];
 
-    } else if (instruction[0] == '2') {
+    }
+    else if (instruction[0] == '2') {
         reg[instruction[1] - '0'] = instruction.substr(2, 2);
-    } else if (instruction[0] == '3') {
+    }
+    else if (instruction[0] == '3') {
         if (instruction.substr(2, 2) == "00") cout << "Register " << instruction[1] - '0' << ": " << reg[instruction[1] - '0'] << endl;
+        else {
+            stringstream ss(instruction.substr(2, 2));
+            if (ss.str().size() == 1) {
+                ss.str("0" + ss.str());
+            }
+            if (mem[ss.str()].size() > 2)
+                mem[ss.str()].erase(0, 2);
+             mem[ss.str()] = reg[instruction[1] - '0'];
+        }
     }
     if (instruction == "C000") {
         cout << "Closing ...";
