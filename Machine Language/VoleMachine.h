b03bc05b49729b8777ec5944c0dd8828acd664fe
GodZilla57 <148370503+GodZilla57@users.noreply.github.com>
2023-11-12 11:56:38 +0200

    Update VoleMachine.h

--- Machine Language/VoleMachine.h
+++ Machine Language/VoleMachine.h
@@ -8,68 +8,9 @@ class Machine {
     map<string, string> mem;
 
 public:
-    void loadFile(const string &filename) {
-        ifstream file(filename);
-        fillMemory(filename);
-        if (file.is_open()) {
-            string line;
-            while (getline(file, line)) {
-                processInstruction(line);
-            }
-            file.close();
-        }
-    }
-
-    void fillMemory(const string &filename) {
-        ifstream file(filename);
-        if (file.is_open()) {
-            string line;
-            string PC = "00";
-            while (getline(file, line)) {
-
-                mem[PC] = line;
-                int pcValue = stoi(PC, nullptr, 16);// Convert the hex string to an integer
-                pcValue += 2;                       // Increment the value
-                stringstream ss;
-                ss << std::hex << pcValue;// Convert the integer back to hex
-                PC = ss.str();
-
-                while (PC.size() < 2) {
-                    PC = "0" + PC;// Ensure two characters in PC
-                }
-                if(PC[1]>=97 and PC[1]<=122){
-                    PC[1]-=32;
-                }
-            cout<<PC<<endl;
-            }
-            file.close();
-        }
-    }
-
-
-    void processInstruction(string &instruction) {
-        instruction = instruction.substr(3);
-        if (instruction[0] == '1') {
-            int location;
-            stringstream ss(instruction.substr(2, 2));
-
-            if (ss.str().size() == 1) {
-                ss.str("0" + ss.str());
-            }
-            if (mem[ss.str()].size() > 2)
-                mem[ss.str()].erase(0, 2);
-            reg[instruction[1] - '0'] = mem[ss.str()];
-
-        } else if (instruction[0] == '2') {
-            reg[instruction[1] - '0'] = instruction.substr(2, 2);
-        } else if (instruction[0] == '3') {
-            if (instruction.substr(2, 2) == "00") cout << "Register " << instruction[1] - '0' << ": " << reg[instruction[1] - '0'] << endl;
-        }
-        if (instruction == "C000") {
-            cout << "Closing ...";
-            return;
-        }
-    }
+    void loadFile(const string &filename);
+    void fillMemory(const string &filename);
+    void processInstruction(string &instruction);
 };
 
 #endif//MACHINE_LANGUAGE_VOLEMACHINE_H
