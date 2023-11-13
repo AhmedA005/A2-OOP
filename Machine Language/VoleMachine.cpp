dfed9ba06b2a63b31615a53677d8e3c48d164bee
GodZilla57 <148370503+GodZilla57@users.noreply.github.com>
2023-11-13 16:28:52 +0200

    Update VoleMachine.cpp

--- Machine Language/VoleMachine.cpp
+++ Machine Language/VoleMachine.cpp
@@ -6,8 +6,9 @@ void Machine::loadFile(const string &filename) {
     fillMemory(filename);
     if (file.is_open()) {
         string line;
+        int jump =0;
         while (getline(file, line)) {
-            processInstruction(line);
+            processInstruction(line,jump);
         }
         file.close();
     }
@@ -39,43 +40,49 @@ void Machine::fillMemory(const string &filename) {
     }
 }
 
-void Machine::processInstruction(string &instruction) {
-    instruction = instruction.substr(3);
-    if (instruction[0] == '1') {
-        int location;
-        stringstream ss(instruction.substr(2, 2));
-
-        if (ss.str().size() == 1) {
-            ss.str("0" + ss.str());
-        }
-       if (mem[ss.str()].size() > 2)
-            mem[ss.str()].erase(0, 2);
-       if(mem[ss.str()][0] == ' ')
-           mem[ss.str()].erase(0,1);
-        reg[instruction[1] - '0'] = mem[ss.str()];
-
-    }
-    else if (instruction[0] == '2') {
-        reg[instruction[1] - '0'] = instruction.substr(2, 2);
-    }
-    else if (instruction[0] == '3') {
-        if (instruction.substr(2, 2) == "00") cout << "Register " << instruction[1] - '0' << ": " << reg[instruction[1] - '0'] << endl;
-        else {
+void Machine::processInstruction(string &instruction , int &jump) {
+    string pc = instruction.substr(0,2);
+    int pcValue = stoi(pc, nullptr, 16);
+    if(pcValue >= jump) {
+        instruction = instruction.substr(3);
+        if (instruction[0] == '1') {
             stringstream ss(instruction.substr(2, 2));
+
             if (ss.str().size() == 1) {
                 ss.str("0" + ss.str());
             }
             if (mem[ss.str()].size() > 2)
                 mem[ss.str()].erase(0, 2);
-             mem[ss.str()] = reg[instruction[1] - '0'];
+            if (mem[ss.str()][0] == ' ')
+                mem[ss.str()].erase(0, 1);
+            reg[instruction[1] - '0'] = mem[ss.str()];
+
+        } else if (instruction[0] == '2') {
+            reg[instruction[1] - '0'] = instruction.substr(2, 2);
+        } else if (instruction[0] == '3') {
+            if (instruction.substr(2, 2) == "00")
+                cout << "Register " << instruction[1] - '0' << ": " << reg[instruction[1] - '0'] << endl;
+            else {
+                stringstream ss(instruction.substr(2, 2));
+                if (ss.str().size() == 1) {
+                    ss.str("0" + ss.str());
+                }
+                if (mem[ss.str()].size() > 2)
+                    mem[ss.str()].erase(0, 2);
+                mem[ss.str()] = reg[instruction[1] - '0'];
+            }
+        } else if (instruction[0] == '4') {
+            reg[instruction[3] - '0'] = reg[instruction[2] - '0'];
+            reg[instruction[2] - '0'] = "";
+        } else if (instruction[0] == 'B') {
+            if (reg[instruction[1] - '0'] == reg[0]) {
+                string j = instruction.substr(2);
+                jump = stoi(j, nullptr, 16);
+            }
         }
-    }
-    else if (instruction[0] == '4'){
-        reg[instruction[3] - '0'] = reg[instruction[2] - '0'];
-        reg[instruction[2] - '0'] = "";
-    }
-    if (instruction == "C000") {
-        cout << "Closing ...";
-        return;
+        if (instruction == "C000") {
+            cout << "Closing ...";
+            return;
+        }
     }
 }
