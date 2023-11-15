5db264eef679d983659da59c7ec590feef61d425
AhmedA005 <117976612+AhmedA005@users.noreply.github.com>
2023-11-15 18:33:05 +0200

    Update VoleMachine.h

--- Machine Language/VoleMachine.h
+++ Machine Language/VoleMachine.h
@@ -2,23 +2,24 @@
 using namespace std;
 #ifndef MACHINE_LANGUAGE_VOLEMACHINE_H
 #define MACHINE_LANGUAGE_VOLEMACHINE_H
+
 class Register {
     vector<string> reg;
-
 public:
     Register(int size);
     const string &getValue(int index);
-    void setValue(int index, const string& value);
-
+    void setValue(int index, const std::string &value);
 };
 
 class Memory {
     map<string, string> mem;
 
 public:
-    const string& getMemory(const string& address);
-    void setMemory(const string& address, const string& value);
+    const string &getMemory(const string &address);
+    map<string, string> getMemory() const;
+    void setMemory(const std::string &address, const std::string &value);
 };
+
 class Machine {
     Register reg{16};
     Memory mem;
@@ -28,9 +29,7 @@ public:
     void loadFile(const string &filename);
     void fillMemory(const string &filename);
     void processInstruction();
-    friend void printMem();
-    friend void printReg(Register&reg);
-
+    void displayMemory();
 };
 
 #endif//MACHINE_LANGUAGE_VOLEMACHINE_H
