4d76f38b999aba7d08aafe759af9d5cc3316e4b3
AhmedA005 <117976612+AhmedA005@users.noreply.github.com>
2023-11-15 06:21:01 +0200

    Update VoleMachine.cpp

--- Machine Language/VoleMachine.cpp
+++ Machine Language/VoleMachine.cpp
@@ -1,4 +1,5 @@
 #include "VoleMachine.h"
+
 vector<string> Lines;
 
 void Machine::loadFile(const string &filename) {
@@ -12,25 +13,25 @@ void Machine::loadFile(const string &filename) {
         file.close();
     }
 }
+
 void Machine::fillMemory(const string &filename) {
     ifstream file(filename);
     if (file.is_open()) {
         string line;
         PC = "00";
         while (getline(file, line)) {
-            mem[PC] = line;
-            int pcValue = stoi(PC, nullptr, 16);// Convert the hex string to an integer
-            pcValue += 2;                       // Increment the value
+            mem.setMemory(PC, line);
+            int pcValue = stoi(PC, nullptr, 16);
+            pcValue += 2;
             stringstream ss;
-            ss << std::hex << pcValue;// Convert the integer back to hex
+            ss << hex << pcValue;
             PC = ss.str();
             while (PC.size() < 2) {
-                PC = "0" + PC;// Ensure two characters in PC
+                PC = "0" + PC;
             }
-            if(PC[1]>=97 and PC[1]<=122){
-                PC[1]-=32;
+            if (PC[1] >= 97 and PC[1] <= 122) {
+                PC[1] -= 32;
             }
-
         }
         file.close();
     }
@@ -40,63 +41,65 @@ void Machine::processInstruction() {
     string ins;
     for (int i = 0; i < Lines.size(); ++i) {
         ins = Lines[i].substr(3);
-        if (ins[0] == '1')
-        {
+        if (ins[0] == '1') {
             string ss = ins.substr(2, 2);
             if (ss.size() == 1) {
                 ss = "0" + ss;
             }
-            if (mem[ss].size() > 2)
-                mem[ss].erase(0, 2);
-            if (mem[ss][0] == ' ')
-                mem[ss].erase(0, 1);
-            reg[ins[1] - '0'] = mem[ss];
-        }
-        else if (ins[0] == '2')
-        {
-            reg[ins[1] - '0'] = ins.substr(2, 2);
-        }
-        else if (ins[0] == '3')
-        {
-            if (ins.substr(2, 2) == "00") cout << "Register " << ins[1] - '0' << ": " << reg[ins[1] - '0'] << endl;
+            if (mem.getMemory(ss).size() > 2) {
+                string temp = mem.getMemory(ss);
+                temp.erase(0, 2);
+                mem.setMemory(ss, temp);
+            }
+
+            if (mem.getMemory(ss)[0] == ' ') {
+                string temp = mem.getMemory(ss);
+                temp.erase(0, 1);
+                mem.setMemory(ss, temp);
+            }
+            reg.setValue(ins[1] - '0', mem.getMemory(ss));
+        } else if (ins[0] == '2') {
+            reg.setValue(ins[1] - '0', ins.substr(2, 2));
+        } else if (ins[0] == '3') {
+            if (ins.substr(2, 2) == "00")
+                cout << "Register " << ins[1] - '0' << ": " << reg.getValue(ins[1] - '0') << endl;
             else {
                 string ss = ins.substr(2, 2);
                 if (ss.size() == 1) {
                     ss = "0" + ss;
                 }
-                if (mem[ss].size() > 2)
-                    mem[ss].erase(0, 2);
-                mem[ss] = reg[ins[1] - '0'];
+                if (mem.getMemory(ss).size() > 2) {
+                    string temp = mem.getMemory(ss);
+                    temp.erase(0, 2);
+                    mem.setMemory(ss, temp);
+                }
+                mem.setMemory(ss, reg.getValue(ins[1] - '0'));
             }
-        }
-        else if (ins[0] == '4')
-        {
-            reg[ins[3] - '0'] = reg[ins[2] - '0'];
-            reg[ins[2] - '0'] = "";
-        }
-        else if(ins[0] == '5') {
-            int x = stoi(reg[ins[2] - '0'], nullptr, 16);
-            int y = stoi(reg[ins[3] - '0'], nullptr, 16);
+        } else if (ins[0] == '4') {
+            reg.setValue(ins[3] - '0', reg.getValue(ins[2] - '0'));
+            reg.setValue(ins[2] - '0', "");
+        } else if (ins[0] == '5') {
+            int x = stoi(reg.getValue(ins[2] - '0'), nullptr, 16);
+            int y = stoi(reg.getValue(ins[3] - '0'), nullptr, 16);
             int sum = x + y;
-            string s = bitset< 64 > (sum).to_string();
-            reverse(s.begin(),s.end());
-            string s2 = s.substr(0,8);
-            reverse(s2.begin(),s2.end());
-            int final = stoi(s2,0,2);
+            string s = bitset<64>(sum).to_string();
+            reverse(s.begin(), s.end());
+            string s2 = s.substr(0, 8);
+            reverse(s2.begin(), s2.end());
+            int final = stoi(s2, 0, 2);
             stringstream hex_sum;
-            hex_sum << std::hex << final;
+            hex_sum << hex << final;
             string ans = hex_sum.str();
 
             while (ans.size() < 2) {
-                ans = "0" + ans;// Ensure two characters in PC
+                ans = "0" + ans;
             }
-            if(ans[1]>=97 and ans[1]<=122){
-                ans[1]-=32;
+            if (ans[1] >= 97 and ans[1] <= 122) {
+                ans[1] -= 32;
             }
-            reg[ins[1] - '0'] = ans;
-        }
-        else if (ins[0] == 'B') {
-            if (reg[0] == reg[ins[1] - '0']) {
+            reg.setValue(ins[1] - '0', ans);
+        } else if (ins[0] == 'B') {
+            if (reg.getValue(0) == reg.getValue(ins[1] - '0')) {
                 string ss = ins.substr(2, 2);
                 PC = ss;
                 for (int j = 0; j < Lines.size(); ++j) {
@@ -112,4 +115,22 @@ void Machine::processInstruction() {
             return;
         }
     }
+}
+
+const string &Memory::getMemory(const string &address) {
+    return mem.at(address);
+}
+
+void Memory::setMemory(const string &address, const string &value) {
+    mem[address] = value;
+}
+
+const string &Register::getValue(int index) {
+    return reg[index];
+}
+
+Register::Register(int size): reg(size,"") {}
+
+void Register::setValue(int index, const string &value) {
+    reg[index] = value;
 }
