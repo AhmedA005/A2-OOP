4e480c7482ec1cf4584f2ef7a5563b92755a9b6b
memo1549 <114335298+memo1549@users.noreply.github.com>
2023-11-15 16:56:12 +0200

    Update VoleMachine.h

--- Machine Language/VoleMachine.h
+++ Machine Language/VoleMachine.h
@@ -8,7 +8,8 @@ class Register {
 public:
     Register(int size);
     const string &getValue(int index);
-    void setValue(int index, const std::string& value);
+    void setValue(int index, const string& value);
+
 };
 
 class Memory {
@@ -16,7 +17,7 @@ class Memory {
 
 public:
     const string& getMemory(const string& address);
-    void setMemory(const std::string& address, const std::string& value);
+    void setMemory(const string& address, const string& value);
 };
 class Machine {
     Register reg{16};
@@ -27,6 +28,9 @@ public:
     void loadFile(const string &filename);
     void fillMemory(const string &filename);
     void processInstruction();
+    friend void printMem();
+    friend void printReg(Register&reg);
+
 };
 
 #endif//MACHINE_LANGUAGE_VOLEMACHINE_H
